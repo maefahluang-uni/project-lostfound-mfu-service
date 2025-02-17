3ebca4cbb8d42a4d5a2e96c0cfc1e0ab4c308161
khantNyarKoKo02 <6631503061@lamduan.mfu.ac.th>
2025-02-17 09:55:53 +0700

    impl: user-crud

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,6 +1,18 @@
 import { Router } from "express";
-import { sigupUserController } from "../controllers/userController";
+import {
+  changePasswordController,
+  getUserController,
+  logoutUserController,
+  signinUserController,
+  sigupUserController,
+  updateUserController,
+} from "../controllers/userController";
 
 export const UserRouter = Router();
 
 UserRouter.post("/signup", sigupUserController);
+UserRouter.post("/signin", signinUserController);
+UserRouter.post("/logout", logoutUserController);
+UserRouter.get("/user/:uid", getUserController);
+UserRouter.put("/user/:uid", updateUserController);
+UserRouter.post("/change-password", changePasswordController);
